86663ea92e92ee441aa0119ffa21cdb4599af1a2
vadlavinay <vadlavinay9494@gmail.com>
2025-08-24 23:53:10 +0530

    use vite_API_base for backend url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const API_BASE = import.meta.env.VITE_API_BASE;
+
 const API = axios.create({
-  baseURL: "http://127.0.0.1:5000/",
+  baseURL: API_BASE, // dynamic URL
 });
 
 export const uploadFile = (file) => {
